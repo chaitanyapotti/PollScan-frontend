40443c5fecb52d069650433b7a4276c30565f67d
Vinay Bagul <vinay@yun.buzz>
2018-10-15 13:13:02 +0530

    server to client end routing handled

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const path = require("path");
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 
-app.use('/', express.static('build'))
+// app.use('/', express.static('build'))
 
-app.use('/*', function(req, res){
-    res.redirect('/')
-})
+// app.use('/*', function(req, res){
+//     res.redirect('/')
+// })
 
-app.listen(2019, () => console.log('pollscan.io on port 2019!'))
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+app.listen(2019, () => console.log("pollscan.io on port 2019!"));
