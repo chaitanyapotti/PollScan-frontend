551bdba45f56f1ffdc6b02053b3f2c0a0217d7b1
chaitanyapotti <chaitanya@electus.network>
2019-01-05 21:34:20 +0530

    created build

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  api_base_url: "http://localhost:2018"
+  // api_base_url: "http://localhost:2018"
   // api_base_url : 'http://35.200.188.2:2018'
-  // api_base_url : 'https://api.pollscan.io'
+  api_base_url: "https://api.pollscan.io"
 };
